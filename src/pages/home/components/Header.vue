9979577578f3cd9c061a056a7347c2cf24a56670
AjingEi <15868872751@163.com>
2019-08-29 15:39:18 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
         <span class="iconfont">&#xe63c;</span>
         输入城市/景点/游戏
     </div>
-    <div class="header-right">
-         {{this.city}}
-         <span class="iconfont arrow-icon">&#xe65c;</span>
-    </div>
+    <router-link to='/city'>
+      <div class="header-right">
+          {{this.city}}
+          <span class="iconfont arrow-icon">&#xe65c;</span>
+      </div>
+    </router-link>
 </div>
 </template>
 
@@ -27,7 +29,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -50,6 +52,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arrow-icon
         margin-left: -.04rem
         font-size: .40rem
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
 
 Vue.use(Router)
 
 // 导出一组路由配置项
 export default new Router({
   // 当你访问根路径时，对应home组件
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Home',
       component: Home
-    }
-  ]
+    }, {
+      path: '/city',
+      name: 'City',
+      component: City
+    }]
 })

08fe5ccad411360242055ca948ad53d30c63c6cd
AjingEi <15868872751@163.com>
2019-08-30 17:10:40 +0800

    performance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :hotCities='hotCities' :cities='cities'></city-list>
-    <city-alphabet :cities='cities'></city-alphabet>
+    <city-list :hotCities='hotCities' :cities='cities' :letter='letter'></city-list>
+    <city-alphabet :cities='cities' @change='handleLetterChange'></city-alphabet>
   </div>
 </template>
 
@@ -24,7 +24,8 @@ export default {
     data () {
       return {
         hotCities: [],
-        cities: {}
+        cities: {},
+        letter: ''
       }
     },
     methods: {
@@ -38,6 +39,9 @@ export default {
           this.hotCities = data.hotCities
           this.cities = data.cities
         }
+      },
+      handleLetterChange (letter) {
+        this.letter = letter
       }
     },
     mounted () {

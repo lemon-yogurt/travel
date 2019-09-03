5c28106dd9caacd89e7ee38d5626f731064998c8
AjingEi <15868872751@163.com>
2019-09-03 17:18:00 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class='banner' @click='handleBannerClick'>
+        <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="">
+        <div class="banner-info">
+            <div class='banner-title'>
+                大连圣亚海洋世界(AAAA景区)
+            </div>
+            <div class='banner-number'>
+                <span class="iconfont banner-icon">&#xe606;</span>
+                39
+            </div>
+        </div>
+    </div>
+    <common-gallary
+      :imgs='imgs'
+      v-show='showGallary'
+      @close='handleGallaryClose'
+    ></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+    name: 'DetailBanner',
+    components: {
+        CommonGallary
+    },
+    data () {
+      return {
+        imgs: ['http://img1.qunarzz.com/sight/p0/1907/ca/ca68bd95bc36bdba3.img.jpg_r_800x800_33169c4b.jpg', 'http://img1.qunarzz.com/sight/p0/1907/ca/ca68bd95bc36bdba3.img.jpg_r_800x800_33169c4b.jpg'],
+        showGallary: false
+      }
+    },
+    methods: {
+        handleBannerClick () {
+            this.showGallary = true
+        },
+        handleGallaryClose () {
+            this.showGallary = false
+        }
+    }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      wodth: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height : .6rem
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      color: #fff
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
